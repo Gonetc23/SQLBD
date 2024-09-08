8af5b0f66c0f9881c31574d8a76d2b419ad838cc
Gonetc23 <46750909+Gonetc23@users.noreply.github.com>
2024-09-08 14:50:37 +0300

    Update SQLCategory.sql

--- SQLCategory.sql
+++ SQLCategory.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Category (
-	id INT PRIMARY KEY,
+
+	id INT IDENTITY(1,1) PRIMARY KEY ,
 	name NVARCHAR(255) NOT NULL
 	
 );
 
 INSERT INTO Category 
-VALUES  (1, 'Мясо'),
-        (2, 'Рыба'),
-		(3, 'Овощи');
+VALUES  ( 'Мясо'),
+        ( 'Рыба'),
+	( 'Овощи');
