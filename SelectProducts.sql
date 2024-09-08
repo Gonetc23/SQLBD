13ce0e0df984f621c3807c102983b0972a936d24
Gonetc23 <46750909+Gonetc23@users.noreply.github.com>
2024-09-08 14:51:32 +0300

    SelectProducts.sql

--- SelectProducts.sql
+++ SelectProducts.sql
@@ -1,5 +1,5 @@
 select prod.name [Имя продукта], cat.name [Имя категории]
 from Products prod
-left join ProductsCategory prodcat on prod.id = prodcat.products_id
-inner join Category cat on cat.id = prodcat.category_id
+left join [dbo].[CategoryProducts] as prodcat on prod.id = prodcat.[product_id] 
+left join  Category as cat  on cat.id = prodcat.[categoty_id]
 order by prod.name
